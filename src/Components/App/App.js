950b3c5b7d755737d8e12a2117c11b9044c21887
Kate McFaul <katemcfaul@gmail.com>
2018-10-28 10:19:58 -0700

    hook up to Spotify api, login, search for similar artists

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import Spotify from '../../util/Spotify';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artists: []
+    }
+  }
+
+  componentDidMount() {
+    const checkWindow = window.location.href.match(/access_token=([^&]*)/);
+    if (checkWindow !== null) {
+      this.searchForSimilarArtists(sessionStorage.getItem("artistID"));
+    }
+  }
+
+  searchForSimilarArtists = (artistID) => {
+    sessionStorage.setItem("artistID", artistID);
+    Spotify.getSimilarArtists(artistID).then(artists => {
+      this.setState({
+        artists: artists
+      })
+    })
+  }
+  
   render() {
+    const artists = this.state.artists.map(artist => <li key={artist.id}>{artist.name}</li>);
+
     return (
-      <p>Hello World</p>
+      <div>
+        <p onClick={() => this.searchForSimilarArtists('43ZHCT0cAZBISjO8DG9PnE')}>Search for Similar Artists</p>
+        <ul>
+          {artists}
+        </ul>
+      </div>
     );
   }
 }
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,74 @@
 import secret from './secret';
 
 const clientID = secret.clientID;
+const redirectLink = secret.redirectLink;
+
+const spotifyLink = `https://api.spotify.com/v1`;
+const scopes = `user-modify-playback-state%20playlist-modify-public`;
+
+let accessToken = '';
+let expires_in;
 
 let Spotify = {
 
+  getSimilarArtists(artistID) {
+    const link = `${spotifyLink}/artists/${artistID}/related-artists`;
+
+    return this.fetchGET(link).then(jsonResponse => {
+      if (jsonResponse) {
+        return jsonResponse.artists.map(artist => {
+          return {
+            id: artist.id,
+            name: artist.name
+          }
+        })
+      } else {
+        return []
+      }
+    })
+  },
+
+  // get accesstoken from variable or url
+  // if no accesstoken, refresh page
+  getAccessToken() {
+    // check if url contains access token
+    const checkToken = window.location.href.match(/access_token=([^&]*)/);
+    if (accessToken) {
+      return accessToken;
+    } else if (checkToken) {
+      this.getTokenInfo() // Make this a promise so access token doesn't return before it's done?
+      return accessToken;
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectLink}&scope=${scopes}&response_type=token&state=state`
+    }
+  },
+
+  // get token and epiration time
+  getTokenInfo() {
+    accessToken = window.location.href.match(/access_token=([^&]*)/)[1];
+    expires_in = window.location.href.match(/expires_in=([^&]*)/)[1];
+    window.setTimeout(() => accessToken = '', expires_in * 1000);
+    window.history.pushState('Access Token', null, '/');
+    return accessToken;
+  },
+
+  // reusable fetch get code, returns json response
+  fetchGET(link) {
+    const headers = this.getHeaders();
+    return fetch(link, { headers: headers }).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+    });
+  },
+
+  // headers for fetch get
+  getHeaders() {
+    return {
+      'Authorization': `Bearer ${this.getAccessToken()}`,
+      'Content-Type': 'application/json'
+    };
+  }
 };
 
 export default Spotify;
